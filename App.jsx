a9de2d6fca38551215d4d322d706b81d71a00f94
debmalyajash <debmalya.jash@gmail.com>
2017-03-22 14:59:52 +0800

    Generate Random Number

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor() {
+      super();
+      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+   };
+
+   forceUpdateHandler() {
+      this.forceUpdate();
+   };
+
    render() {
       return (
          <div>
-            Hello to React!!!
+            <button onClick = {this.forceUpdateHandler}>Generate Random Number</button>
+            <h4>Random number: {parseInt(Math.random()*1000000)}</h4>
          </div>
       );
    }
